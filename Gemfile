916a9bb10c43fe7b9eb65765642cd4b24c45a2ae
Dmitry Chernyshov <dmitry@razortheory.com>
2016-01-27 21:25:34 +0300

    Big refactoring

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+group :development do
+  gem 'guard-sass', :require => false
+  gem 'guard'
+  gem 'guard-livereload'
+end
--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,12 @@
 $(document).ready(function (){
   document.session = $('#session').val();
+  msgInput = $('#message-text');
 
   setTimeout(requestMessage, 100);
 
   $(document).keypress(function(e) {
       if(e.which == 13) {
-          text = $('#message-text').val();
+          text = msgInput.val();
           if(text != ''){
             $.ajax({
               url: '/message',
@@ -14,8 +15,8 @@ $(document).ready(function (){
                 session: document.session,
                 text: text
               },
-              success: function(data, status, xhr){
-                console.log('sent');
+              beforeSend: function (){
+                msgInput.val('');
               }
             });
           }
@@ -25,15 +26,41 @@ $(document).ready(function (){
 });
 
 function requestMessage(){
-  console.log('request update!');
+  var msgsBox = $('#messages-box');
+
 	$.getJSON('/message/update', {session: document.session},
 		function(data, status, xhr) {
-      console.log(data['message']);
-      date = new Date();
-      nice_date = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-      formatted = '<div class="message">' + '<span class="date">' + nice_date + '</span> ' + data['text'] + '</div>';
-      $('#messages').append(formatted);
+      date = getCurrentTime();
+      formatted = '<div class="msg-wrapper">' +
+                    '<div class="msg-user-box">' +
+                      '<div class="msg-user-icon"></div>' +
+                    '</div>' +
+                    '<div class="msg">' + date + '<br>' + data['text'] + '</div>' +
+                  '</div>';
+
+      msgsBox.append(formatted);
+      msgsBoxHeight = msgsBox.prop('scrollHeight');
+      msgsBox.scrollTop(msgsBoxHeight);
+
 			setTimeout(requestMessage, 0);
 		}
 	);
+}
+
+function getCurrentTime(){
+  date = new Date();
+  hours = date.getHours();
+  minutes = date.getMinutes();
+  seconds = date.getSeconds();
+  hours = addZero(hours);
+  minutes = addZero(minutes);
+  seconds = addZero(seconds);
+  return hours + ':' + minutes + ':' + seconds;
+}
+
+function addZero(time_part){
+  if (time_part < 10){
+    return '0' + time_part;
+  }
+  return time_part;
 }
